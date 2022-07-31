2817af410273f03b822de5fb13b3498af98a3d71
Aahad Ali <104500845+Aahad-Ali@users.noreply.github.com>
2022-07-31 04:11:48 -0700

    Add files via upload

---
+++ 3rd javascriprt/app.js
@@ -0,0 +1,11 @@
+var age = prompt("Enter your age");
+// alert("I am " + age + " " + "years old");
+if(age >= 18){
+ alert("You are eligible to use this web");
+}
+if(age < 18){
+    alert("You are not eligible too use this web");
+}
+
+var visit = prompt("How many time you visit this site");
+alert("Today you visit this web" +" " + visit + " " + "time");
